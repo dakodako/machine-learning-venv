45c844eb479e23d19398dea1ab8c92a6e8dec393
dako <chididi_gloria@hotmail.com>
2019-04-15 10:39:15 +1000

    Update open-nii-images.py

--- open-nii-images.py
+++ open-nii-images.py
@@ -26,6 +26,7 @@ from matplotlib import pyplot as plt
 #%%
 
 ff = glob.glob('../MRI_data/dataset/*')
+#ff = glob.glob('dataset/T1/*')
 
 
 
@@ -64,7 +65,7 @@ m = np.max(images)
 mi = np.min(images)
 images = (images - mi)/(m - mi)
 #%%
-temp = np.zeros([32*51,116,116,1])
+temp = np.zeros([12*51,116,116,1])
 temp[:,3:,3:,:] = images
 
 images = temp
@@ -99,6 +100,56 @@ input_img = Input(shape = (x, y, inChannel))
 
 # there are two parts in the autoencoder: encoder and decoder
 #%%
+def unet2(input_img):
+	#s = Lambda(lambda x: x/255)(input_img)
+	c1 = Conv2D(32,(3,3),activation = 'relu', padding = 'same')(input_img)
+	c1 = Dropout(0.1)(c1) # ????
+	c1 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(c1)
+	p1 = MaxPooling2D((2,2), strides = (2,2))(c1)
+	c2 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(p1)
+	c2 = Dropout(0.1)(c2) # ????
+	c2 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c2)
+	p2 = MaxPooling2D((2,2), strides = (2,2))(c2)
+
+	c3 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(p2)
+	c3 = Dropout(0.1)(c3) # ????
+	c3 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(c3)
+	p3 = MaxPooling2D((2,2), strides = (2,2))(c3)
+
+	c4 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(p3)
+	c4 = Dropout(0.1)(c4) # ????
+	c4 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(c4)
+	p4 = MaxPooling2D((2,2), strides = (2,2))(c4)
+
+	c5 = Conv2D(512, (3,3),activation = 'relu', padding = 'same')(p4)
+	c5 = Dropout(0.1)(c5) # ????
+	c5 = Conv2D(512, (3,3),activation = 'relu', padding = 'same')(c5)
+
+	u6 = Conv2DTranspose(256,(2,2), strides = (2,2), padding = 'same')(c5)
+	u6 = concatenate([u6, c4])
+	c6 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(u6)
+	c6 = Dropout(0.1)(c6)
+	c6 = Conv2D(256,(3,3), activation = 'relu', padding = 'same')(c6)
+	u7 = Conv2DTranspose(256, (3,3), strides = (2,2), padding = 'valid')(c6)
+	u7 = concatenate([u7,c3])
+	c7 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(u7)
+	c7 = Dropout(0.1)(c7)
+	c7 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(c7)
+
+	u8 = Conv2DTranspose(64, (2,2), strides = (2,2), padding = 'same')(c7)
+	u8 = concatenate([u8, c2])
+	c8 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(u8)
+	c8 = Dropout(0.1)(c8)
+	c8 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c8)
+
+	u9 = Conv2DTranspose(32, (2,2), strides = (2,2), padding = 'same')(c8)
+	u9 = concatenate([u9, c1])
+	c9 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(u9)
+	c9 = Dropout(0.1)(c9)
+	c9 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(c9)
+	output = Conv2D(1,(1,1), activation = 'relu', padding = 'same')(c9) 
+	return output
+
 def unet(input_img):
 	s = Lambda(lambda x: x/255)(input_img)
 	c1 = Conv2D(64,(3,3),activation = 'relu', padding = 'same')(s)
@@ -189,11 +240,10 @@ def autoencoder(input_img):
 
 #%%
 #autoencoder = Model(input_img, autoencoder(input_img))
-autoencoder = Model(input_img, unet(input_img))
+autoencoder = Model(input_img, unet2(input_img))
 autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
 
 autoencoder.summary()
-
 
 autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_ground))
 loss = autoencoder_train.history['loss']
