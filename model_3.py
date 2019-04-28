eeb1ab971b25e6e46debb94c0471baba66fabcf2
dako <chididi_gloria@hotmail.com>
2019-04-29 00:16:21 +1000

    model_3

--- model_3.py
+++ model_3.py
@@ -26,6 +26,107 @@ import math
 import glob
 from matplotlib import pyplot as plt
 import sys
+#%%
+def unet2(input_img):
+	#s = Lambda(lambda x: x/255)(input_img)
+	c1 = Conv2D(32,(3,3),activation = 'relu', padding = 'same')(input_img)
+	c1 = Dropout(0.1)(c1) # ????
+	c1 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(c1)
+	p1 = MaxPooling2D((2,2), strides = (2,2))(c1)
+	c2 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(p1)
+	c2 = Dropout(0.1)(c2) # ????
+	c2 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c2)
+	p2 = MaxPooling2D((2,2), strides = (2,2))(c2)
+
+	c3 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(p2)
+	c3 = Dropout(0.1)(c3) # ????
+	c3 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(c3)
+	p3 = MaxPooling2D((2,2), strides = (2,2))(c3)
+
+	c4 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(p3)
+	c4 = Dropout(0.1)(c4) # ????
+	c4 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(c4)
+	p4 = MaxPooling2D((2,2), strides = (2,2))(c4)
+
+	c5 = Conv2D(512, (3,3),activation = 'relu', padding = 'same')(p4)
+	c5 = Dropout(0.1)(c5) # ????
+	c5 = Conv2D(512, (3,3),activation = 'relu', padding = 'same')(c5)
+
+	u6 = Conv2DTranspose(256,(2,2), strides = (2,2), padding = 'same')(c5)
+	u6 = concatenate([u6, c4])
+	c6 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(u6)
+	c6 = Dropout(0.1)(c6)
+	c6 = Conv2D(256,(3,3), activation = 'relu', padding = 'same')(c6)
+	u7 = Conv2DTranspose(128, (2,2), strides = (2,2), padding = 'valid')(c6)
+	u7 = concatenate([u7,c3])
+	c7 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(u7)
+	c7 = Dropout(0.1)(c7)
+	c7 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(c7)
+
+	u8 = Conv2DTranspose(64, (2,2), strides = (2,2), padding = 'same')(c7)
+	u8 = concatenate([u8, c2])
+	c8 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(u8)
+	c8 = Dropout(0.1)(c8)
+	c8 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c8)
+
+	u9 = Conv2DTranspose(32, (2,2), strides = (2,2), padding = 'same')(c8)
+	u9 = concatenate([u9, c1])
+	c9 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(u9)
+	c9 = Dropout(0.1)(c9)
+	c9 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(c9)
+	output = Conv2D(1,(1,1), activation = 'relu', padding = 'same')(c9) 
+	return output
+#%%
+def autoencoder2(input_img):
+	#s = Lambda(lambda x: x/255)(input_img)
+	c1 = Conv2D(32,(3,3),activation = 'relu', padding = 'same')(input_img)
+	c1 = Dropout(0.1)(c1) # ????
+	c1 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(c1)
+	p1 = MaxPooling2D((2,2), strides = (2,2))(c1)
+	c2 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(p1)
+	c2 = Dropout(0.1)(c2) # ????
+	c2 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c2)
+	p2 = MaxPooling2D((2,2), strides = (2,2))(c2)
+
+	c3 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(p2)
+	c3 = Dropout(0.1)(c3) # ????
+	c3 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(c3)
+	p3 = MaxPooling2D((2,2), strides = (2,2))(c3)
+
+	c4 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(p3)
+	c4 = Dropout(0.1)(c4) # ????
+	c4 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(c4)
+	p4 = MaxPooling2D((2,2), strides = (2,2))(c4)
+
+	c5 = Conv2D(512, (3,3),activation = 'relu', padding = 'same')(p4)
+	c5 = Dropout(0.1)(c5) # ????
+	c5 = Conv2D(512, (3,3),activation = 'relu', padding = 'same')(c5)
+
+	u6 = Conv2DTranspose(512,(2,2), strides = (2,2), padding = 'same')(c5)
+	#u6 = concatenate([u6, c4])
+	c6 = Conv2D(256, (3,3), activation = 'relu', padding = 'same')(u6)
+	c6 = Dropout(0.1)(c6)
+	c6 = Conv2D(256,(3,3), activation = 'relu', padding = 'same')(c6)
+	u7 = Conv2DTranspose(256, (2,2), strides = (2,2), padding = 'valid')(c6)
+	#u7 = concatenate([u7,c3])
+	c7 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(u7)
+	c7 = Dropout(0.1)(c7)
+	c7 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(c7)
+
+	u8 = Conv2DTranspose(128, (2,2), strides = (2,2), padding = 'same')(c7)
+	#u8 = concatenate([u8, c2])
+	c8 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(u8)
+	c8 = Dropout(0.1)(c8)
+	c8 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c8)
+
+	u9 = Conv2DTranspose(64, (2,2), strides = (2,2), padding = 'same')(c8)
+	#u9 = concatenate([u9, c1])
+	c9 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(u9)
+	c9 = Dropout(0.1)(c9)
+	c9 = Conv2D(32,(3,3), activation = 'relu', padding = 'same')(c9)
+	output = Conv2D(1,(1,1), activation = 'relu', padding = 'same')(c9) 
+	return output
+
 def pad_zero_margins(input_img):
     output_img = np.zeros((256,256))
     output_img[:,31:224] = input_img
@@ -52,14 +153,19 @@ def open_images(filepath):
 
 
 
+#%%
 
-
-filepath_X = sys.argv[1]
-filepath_ground = sys.argv[2]
+filepath_X = 'PETRA2/*'
+filepath_ground = 'MP2RAGE2/*'
 
 image_X = open_images(filepath_X)
 image_ground = open_images(filepath_ground)
 print(image_X.shape)
-filepath_X = sys.argv[1]
-filepath_ground = sys.argv[2]
-images_X = open_images(filepath_X)
+#%%
+inChannel = 1
+x, y = 256, 256
+input_img = Input(shape = (x,y,inChannel))
+autoencoder = Model(input_img, unet2(input_img))
+autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+autoencoder.summary()
+
