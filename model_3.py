7aacdf682491ba7de269b24b74a9c933e8ca7de1
dakodako <chididi_gloria@hotmail.com>
2019-04-29 13:30:03 +1000

    ran model_3

--- model_3.py
+++ model_3.py
@@ -28,6 +28,56 @@ import glob
 from matplotlib import pyplot as plt
 import sys
 #%%
+def unet3(input_img):
+	#s = Lambda(lambda x: x/255)(input_img)
+	c1 = Conv2D(16,(3,3),activation = 'relu', padding = 'same')(input_img)
+	c1 = Dropout(0.1)(c1) # ????
+	c1 = Conv2D(16,(3,3), activation = 'relu', padding = 'same')(c1)
+	p1 = MaxPooling2D((2,2), strides = (2,2))(c1)
+	c2 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(p1)
+	c2 = Dropout(0.1)(c2) # ????
+	c2 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(c2)
+	p2 = MaxPooling2D((2,2), strides = (2,2))(c2)
+
+	c3 = Conv2D(64,(3,3), activation = 'relu', padding = 'same')(p2)
+	c3 = Dropout(0.1)(c3) # ????
+	c3 = Conv2D(64,(3,3), activation = 'relu', padding = 'same')(c3)
+	p3 = MaxPooling2D((2,2), strides = (2,2))(c3)
+
+	c4 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(p3)
+	c4 = Dropout(0.1)(c4) # ????
+	c4 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(c4)
+	p4 = MaxPooling2D((2,2), strides = (2,2))(c4)
+
+	c5 = Conv2D(256, (3,3),activation = 'relu', padding = 'same')(p4)
+	c5 = Dropout(0.1)(c5) # ????
+	c5 = Conv2D(256, (3,3),activation = 'relu', padding = 'same')(c5)
+
+	u6 = Conv2DTranspose(256,(2,2), strides = (2,2), padding = 'same')(c5)
+	u6 = concatenate([u6, c4])
+	c6 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(u6)
+	c6 = Dropout(0.1)(c6)
+	c6 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(c6)
+	u7 = Conv2DTranspose(128, (2,2), strides = (2,2), padding = 'valid')(c6)
+	u7 = concatenate([u7,c3])
+	c7 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(u7)
+	c7 = Dropout(0.1)(c7)
+	c7 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c7)
+
+	u8 = Conv2DTranspose(32, (2,2), strides = (2,2), padding = 'same')(c7)
+	u8 = concatenate([u8, c2])
+	c8 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(u8)
+	c8 = Dropout(0.1)(c8)
+	c8 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(c8)
+
+	u9 = Conv2DTranspose(16, (2,2), strides = (2,2), padding = 'same')(c8)
+	u9 = concatenate([u9, c1])
+	c9 = Conv2D(16,(3,3), activation = 'relu', padding = 'same')(u9)
+	c9 = Dropout(0.1)(c9)
+	c9 = Conv2D(16,(3,3), activation = 'relu', padding = 'same')(c9)
+	output = Conv2D(1,(1,1), activation = 'relu', padding = 'same')(c9) 
+	return output
+#%%
 def unet2(input_img):
 	#s = Lambda(lambda x: x/255)(input_img)
 	c1 = Conv2D(32,(3,3),activation = 'relu', padding = 'same')(input_img)
@@ -180,10 +230,10 @@ def open_images(filepath):
 
 #%%
 
-#filepath_X = sys.argv[1]
-#filepath_ground = sys.argv[2]
-filepath_X = 'PETRA2/*'
-filepath_ground = 'MP2RAGE2/*'
+filepath_X = sys.argv[1]
+filepath_ground = sys.argv[2]
+#filepath_X = 'PETRA2/*'
+#filepath_ground = 'MP2RAGE2/*'
 images_X = open_images(filepath_X)
 images_ground = open_images(filepath_ground)
 print(images_X.shape)
@@ -219,12 +269,12 @@ plt.imshow(curr_img, cmap='gray')
 
 #%%
 
-batch_size = 10
-epochs = 100
+batch_size = 2
+epochs = 500
 inChannel = 1
 x, y = 256, 256
 input_img = Input(shape = (x,y,inChannel))
-autoencoder = Model(input_img, unet2(input_img))
+autoencoder = Model(input_img, unet3(input_img))
 autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
 autoencoder.summary()
 
@@ -233,20 +283,21 @@ autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=batch_size
 loss = autoencoder_train.history['loss']
 val_loss = autoencoder_train.history['val_loss']
 autoencoder.save('autoencoder2_petra.h5')
+'''
 #%%
-filepath_test_X = '../Documents/MRI_data/dataset/X/*'#sys.argv[3]
-#filepath_test_ground = sys.argv[4]
-test_X = open_images2(filepath_test_X)
-#test_ground = open_images(filepath_test_ground)
+filepath_test_X = sys.argv[3]#'../Documents/MRI_data/dataset/X/*'#sys.argv[3]
+filepath_test_ground = sys.argv[4]
+test_X = open_images(filepath_test_X)
+test_ground = open_images(filepath_test_ground)
 
-#pred = autoencoder.predict(test_X)
+pred = autoencoder.predict(test_X)
 plt.figure(figsize=(20, 4))
 print("Test Images inputs")
 for i in range(5):
     plt.subplot(1, 5, i + 1)
     plt.imshow(test_X[i, ..., 0], cmap = 'gray')
 plt.show()
-'''
+
 plt.figure(figsize=(20, 4))
 print("Test Images")
 for i in range(5):
@@ -260,4 +311,6 @@ for i in range(5):
     plt.subplot(1, 5, i+1)
     plt.imshow(pred[i, ..., 0], cmap='gray')  
 plt.show()
+
 '''
+
