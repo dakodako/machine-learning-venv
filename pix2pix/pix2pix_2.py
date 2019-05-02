063226234fc4ed4ca8c78f173a7e13aa62724420
dakodako <chididi_gloria@hotmail.com>
2019-05-02 23:42:22 +1000

    Update pix2pix_2.py

--- pix2pix/pix2pix_2.py
+++ pix2pix/pix2pix_2.py
@@ -8,11 +8,12 @@ from skimage.transform import resize, rotate
 import nibabel as nib 
 import scipy
 from keras.optimizers import Adam, RMSprop
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate, ReLU, MaxPooling2D
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D, Conv2DTranspose
 from keras.models import Sequential, Model
+from keras.layers.merge import concatenate
 #%%
 #from data_loader import DataLoader
 import sys
@@ -168,6 +169,75 @@ class Pix2Pix():
         
         self.combined = Model(inputs = [img_mp2, img_petra], outputs = [valid, fake_mp2])
         self.combined.compile(loss = ['mse','mae'], loss_weights=[1,100],optimizer = optimizer)
+    def build_generator2(self):
+	#s = Lambda(lambda x: x/255)(input_img)
+        input_img = Input(shape = self.img_shape)
+        c1 = Conv2D(16,(3,3),activation = 'relu', padding = 'same')(input_img)
+        c1 = ReLU()(c1)
+        c1 = Dropout(0.1)(c1) # ????
+        c1 = Conv2D(16,(3,3), activation = 'relu', padding = 'same')(c1)
+        c1 = ReLU()(c1)
+        p1 = MaxPooling2D((2,2), strides = (2,2))(c1)
+        c2 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(p1)
+        c2 = ReLU()(c2)
+        c2 = Dropout(0.1)(c2) # ????
+        c2 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(c2)
+        c2 = ReLU()(c2)
+        p2 = MaxPooling2D((2,2), strides = (2,2))(c2)
+
+        c3 = Conv2D(64,(3,3), activation = 'relu', padding = 'same')(p2)
+        c3 = ReLU()(c3)
+        c3 = Dropout(0.1)(c3) # ????
+        c3 = Conv2D(64,(3,3), activation = 'relu', padding = 'same')(c3)
+        c3 = ReLU()(c3)
+        p3 = MaxPooling2D((2,2), strides = (2,2))(c3)
+
+        c4 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(p3)
+        c4 = ReLU()(c4)
+        c4 = Dropout(0.1)(c4) # ????
+        c4 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(c4)
+        c4 = ReLU()(c4)
+        p4 = MaxPooling2D((2,2), strides = (2,2))(c4)
+
+        c5 = Conv2D(256, (3,3),activation = 'relu', padding = 'same')(p4)
+        c5 = ReLU()(c5)
+        c5 = Dropout(0.1)(c5) # ????
+        c5 = Conv2D(256, (3,3),activation = 'relu', padding = 'same')(c5)
+        c5 = ReLU()(c5)
+
+        u6 = Conv2DTranspose(256,(2,2), strides = (2,2), padding = 'same')(c5)
+        u6 = concatenate([u6, c4])
+        c6 = Conv2D(128, (3,3), activation = 'relu', padding = 'same')(u6)
+        c6 = ReLU()(c6)
+        c6 = Dropout(0.1)(c6)
+        c6 = Conv2D(128,(3,3), activation = 'relu', padding = 'same')(c6)
+        c6 = ReLU()(c6)
+        u7 = Conv2DTranspose(128, (2,2), strides = (2,2), padding = 'valid')(c6)
+        u7 = concatenate([u7,c3])
+        c7 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(u7)
+        c7 = ReLU()(c7)
+        c7 = Dropout(0.1)(c7)
+        c7 = Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c7)
+        c7 = ReLU()(c7)
+
+        u8 = Conv2DTranspose(32, (2,2), strides = (2,2), padding = 'same')(c7)
+        u8 = concatenate([u8, c2])
+        c8 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(u8)
+        c8 = ReLU()(c8)
+        c8 = Dropout(0.1)(c8)
+        c8 = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(c8)
+        c8 = ReLU()(c8)
+
+        u9 = Conv2DTranspose(16, (2,2), strides = (2,2), padding = 'same')(c8)
+        u9 = concatenate([u9, c1])
+        c9 = Conv2D(16,(3,3), activation = 'relu', padding = 'same')(u9)
+        c9 = ReLU()(c9)
+        c9 = Dropout(0.1)(c9)
+        c9 = Conv2D(16,(3,3), activation = 'relu', padding = 'same')(c9)
+        c9 = ReLU()(c9)
+        output = Conv2D(1,(1,1), activation = 'relu', padding = 'same')(c9)
+        
+        return Model(input_img, output)
     def build_generator(self):
         '''u-net'''
         def conv2d(layer_input, filters, f_size=4, bn=True):
